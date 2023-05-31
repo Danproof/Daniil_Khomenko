939ed9b4a0e899e8062e7d11852a52bbee958574
Danproof <daniyilkhomenko2003@gmail.com>
2023-05-31 23:47:54 +0300

    Merge branch 'WebUI' of https://github.com/Danproof/Daniil_Khomenko into WebUI

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,3 @@
-# import os
-# import sys
-
-# ROOT = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
-# sys.path.insert(1, ROOT)
-
-
 def before_feature(context, feature):
     '''Will be called each time a new feature is being tested'''
 
